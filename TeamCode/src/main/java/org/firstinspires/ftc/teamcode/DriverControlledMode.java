a57b8b0b9965fe7a191b5d7ce72d03def13fc219
ButterflyNebula <athira.arayath@gmail.com>
2018-10-26 19:41:53 -0400

    Added Vuforia navigation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlledMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlledMode.java
@@ -35,132 +35,127 @@ public class DriverControlledMode extends LinearOpMode {
     private static final double     LIFT_DOWN_SPEED   = 0.6;
     private static final float      mmPerInch         = 25.4f;
     private static final double     SHOULDER_SPEED    = 1.0;
+    boolean  targetVisible = false;
+    OpenGLMatrix  lastLocation = null;
+    List<VuforiaTrackable> allTrackables = null;
 
     @Override public void runOpMode() {
 
         roverRuckusBot.initRobot(hardwareMap);
-        boolean isLocked = true;
-        waitForStart();
+        allTrackables =   roverRuckusBot.getRobotLocation().getAllTrackables();
+
         telemetry.addData("Press Play to Start", "");
         telemetry.update();
+        waitForStart();
 
-        Location loc = roverRuckusBot.getRobotLocation();
-        List<VuforiaTrackable> allTrackables =  loc.getAllTrackables();
-        boolean  targetVisible = false;
-        OpenGLMatrix  lastLocation = null;
+        while (opModeIsActive())
+        {
+                getCurrentPosition();
 
-
-        while (opModeIsActive()) {
-
-            telemetry.addData("Reading current position: ", "");
-            telemetry.update();
-
-            targetVisible = false;
-            for (VuforiaTrackable trackable : allTrackables) {
-                if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
-                    telemetry.addData("Visible Target", trackable.getName());
-                    targetVisible = true;
-
-                    // getUpdatedRobotLocation() will return null if no new information is available since
-                    // the last time that call was made, or if the trackable is not currently visible.
-                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
-                    if (robotLocationTransform != null) {
-                        lastLocation = robotLocationTransform;
-                    }
-                    break;
+                if (gamepad1.dpad_up == true) {
+                    roverRuckusBot.getLiftAssembly().liftUpRobot(LIFT_UP_SPEED);
+                } else if (gamepad1.dpad_down == true) {
+                    roverRuckusBot.getLiftAssembly().lowerRobot(LIFT_DOWN_SPEED);
+                } else {
+                    roverRuckusBot.getLiftAssembly().resetLift();
                 }
-            }
 
-            // Provide feedback as to where the robot is located (if we know).
-            if (targetVisible) {
-                // express position (translation) of robot in inches.
-                VectorF translation = lastLocation.getTranslation();
-                double tX = translation.get(0)/ mmPerInch;
-                double tY = translation.get(1)/ mmPerInch;
-                double tZ = translation.get(2)/ mmPerInch;
-                telemetry.addData("Translation in inches: ", "(" + tX + ", " + tY + ", " + tZ + ")");
+                //forwards
+                if (gamepad1.left_stick_y > 0) {
+                    roverRuckusBot.getChassisAssembly().moveForward(WHEEL_SPEED * gamepad1.left_stick_y);
+                }
+                //backwards
+                else if (gamepad1.left_stick_y < 0) {
+                    roverRuckusBot.getChassisAssembly().moveBackwards(WHEEL_SPEED * -gamepad1.left_stick_y);
+                }
+                //turn right
+                else if (gamepad1.left_stick_x > 0) {
+                    roverRuckusBot.getChassisAssembly().turnRight(WHEEL_SPEED * gamepad1.left_stick_x);
+                }
+                //turn left
+                else if (gamepad1.left_stick_x < 0) {
+                    roverRuckusBot.getChassisAssembly().turnLeft(WHEEL_SPEED * -gamepad1.left_stick_x);
+                }
+                //side right
+                else if (gamepad1.right_trigger > 0) {
+                    roverRuckusBot.getChassisAssembly().sideWaysRight(WHEEL_SPEED * gamepad1.right_trigger);
+                }
+                //side left
+                else if (gamepad1.left_trigger > 0) {
+                    roverRuckusBot.getChassisAssembly().sideWaysLeft(WHEEL_SPEED * gamepad1.left_trigger);
+                } else {
+                    roverRuckusBot.getChassisAssembly().stopMoving();
+                }
 
-                // express the rotation of the robot in degrees.
-                Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
-                double rX = rotation.firstAngle;
-                double rY = rotation.secondAngle;
-                double rZ = rotation.thirdAngle;
-                telemetry.addData("Rotation in deg: ", "(" + rX + ", " + rY + ", " + rZ + ")");
-            }
-            else {
-                telemetry.addData("Visible Target", "none");
-            }
-
-            if (gamepad1.dpad_up == true) {
-                roverRuckusBot.getLiftAssembly().liftUpRobot(LIFT_UP_SPEED);
-            } else if (gamepad1.dpad_down == true) {
-                roverRuckusBot.getLiftAssembly().lowerRobot(LIFT_DOWN_SPEED);
-            } else {
-                roverRuckusBot.getLiftAssembly().resetLift();
-            }
-
-
-            //forwards
-            if (gamepad1.left_stick_y > 0) {
-                roverRuckusBot.getChassisAssembly().moveForward(WHEEL_SPEED * gamepad1.left_stick_y);
-            }
-            //backwards
-            else if (gamepad1.left_stick_y < 0) {
-                roverRuckusBot.getChassisAssembly().moveBackwards(WHEEL_SPEED * -gamepad1.left_stick_y);
-            }
-            //turn right
-            else if (gamepad1.left_stick_x > 0) {
-                roverRuckusBot.getChassisAssembly().turnRight(WHEEL_SPEED * gamepad1.left_stick_x);
-            }
-            //turn left
-            else if (gamepad1.left_stick_x < 0) {
-                roverRuckusBot.getChassisAssembly().turnLeft(WHEEL_SPEED * -gamepad1.left_stick_x);
-            }
-            //side right
-            else if (gamepad1.right_trigger > 0) {
-                roverRuckusBot.getChassisAssembly().sideWaysRight(WHEEL_SPEED * gamepad1.right_trigger);
-            }
-            //side left
-            else if (gamepad1.left_trigger > 0) {
-                roverRuckusBot.getChassisAssembly().sideWaysLeft(WHEEL_SPEED * gamepad1.left_trigger);
-            } else {
-                roverRuckusBot.getChassisAssembly().stopMoving();
-            }
-
-            double power = 0.7;
-            //intake wheels
-            if (gamepad1.x == true)
-            {
-                while (power < 1)
+                double power = 0.7;
+                //intake wheels
+                if (gamepad1.x == true)
                 {
-                    roverRuckusBot.getArmAssembly().Intake(-power);
-                    power = power + 0.1;
+                    while (power < 1)
+                    {
+                        roverRuckusBot.getArmAssembly().Intake(-power);
+                        power = power + 0.1;
+                    }
+                    while (power > 0.7) {
+                        roverRuckusBot.getArmAssembly().Intake(-power);
+                        power = power - 0.1;
+                    }
                 }
-                while (power > 0.6) {
-                    roverRuckusBot.getArmAssembly().Intake(-power);
-                    power = power - 0.1;
+                else
+                {
+                    roverRuckusBot.getArmAssembly().Intake(0);
                 }
-                power = 0.6;
 
-            }
-            else
+                //   if (gamepad1.y == true)
+                // {
+                //   roverRuckusBot.getArmAssembly().OpenArm(0.7,0.5 );
+                //}
+
+                if (gamepad1.y == true) {
+                    roverRuckusBot.getLiftAssembly().unlockRobot(0);
+
+                }
+                if (gamepad1.a== true) {
+                    roverRuckusBot.getLiftAssembly().lockRobot(0.5);
+
+                }
+        }
+    }
+
+    public void getCurrentPosition()
+    {
+        targetVisible = false;
+        for (VuforiaTrackable trackable : allTrackables)
+        {
+            if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible())
             {
-                roverRuckusBot.getArmAssembly().Intake(0);
-            }
+                telemetry.addData("Visible Target", trackable.getName());
+                targetVisible = true;
 
-         //   if (gamepad1.y == true)
-           // {
-             //   roverRuckusBot.getArmAssembly().OpenArm(0.7,0.5 );
-            //}
-
-            if (gamepad1.y == true) {
-                roverRuckusBot.getLiftAssembly().unlockRobot(0);
-
-            }
-            if (gamepad1.a== true) {
-                roverRuckusBot.getLiftAssembly().lockRobot(0.5);
-
+                OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
+                if (robotLocationTransform != null)
+                {
+                    lastLocation = robotLocationTransform;
+                }
+                break;
             }
         }
+
+        if (targetVisible)
+        {
+            //Get the Translational Infomration in Inches
+            VectorF translation = lastLocation.getTranslation();
+            telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                    translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+
+            //Getthe Rotational Information in Degrees
+            Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
+            telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
+        }
+        else
+        {
+            telemetry.addData("Visible Target", "none");
+        }
+        telemetry.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Location.java
@@ -38,12 +38,7 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
 public class Location {
 
     private RobotHardware robotHardware = null;
-    private List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
-    private VuforiaTrackable calc = null;
-    private VuforiaTrackable blueRover = null;
-    private VuforiaTrackable redFootprint = null;
-    private VuforiaTrackable frontCraters =null;
-    private VuforiaTrackable backSpace = null;
+
 
     // Since ImageTarget trackables use mm to specifiy their dimensions, we must use mm for all the physical dimension.
     // We will define some constants and conversions here
@@ -55,185 +50,89 @@ public class Location {
     private OpenGLMatrix lastLocation = null;
     private boolean targetVisible = false;
 
+    //Vuforia Setup
+    private static final String VUFORIA_KEY = "AVsSQB3/////AAABGehU0OjxREoznvNKEBqbvmskci8syRYfMKE0XlaGnZpw68DAZV19s7dfqc0vWrY78bAO2Ym2n1T2rDvNBOVVbMWxXIRo2c18JH6/c2fcKT1bRKxsG7bYq69+n9IHmKedY6rmTU1VOZZdtSTXh7exMsl67IAcnCZ0/ec+P+ZMpkK5v4X8d27rbEigGqqHayGe1/lG2afzgcHY7QxjJ/x5O4yGmVVs8wdzdupke19U+M8Z/x0FcYIfTAHuXcaydEL+h/w/ppcuNarD2ggo2BxdWeOGLx5GOin1yruVfvDAazPEuI0m3yEwXQNZ4e0ar2G0jDCZpAJPJcJRRVttBMwPoAvzTwySUx3qI1eNSJRAH+bk";
+    VuforiaLocalizer vuforia;
+    private List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
+
     public Location (RobotHardware hardware)
     {
         this.robotHardware = hardware;
     }
 
-    public void initializeNavigation()
-    {
 
-        /**
-         * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
-         * localization engine.
-         */
-        VuforiaLocalizer vuforia;
-
-        /*
-         * Vuforia Set Up
-         */
+    public void initializeNavigation() {
+        //Vuforia Setyp
         int cameraMonitorViewId = robotHardware.getHwMap().appContext.getResources().getIdentifier("cameraMonitorViewId", "id", robotHardware.getHwMap().appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-        parameters.vuforiaLicenseKey = "AVsSQB3/////AAABGehU0OjxREoznvNKEBqbvmskci8syRYfMKE0XlaGnZpw68DAZV19s7dfqc0vWrY78bAO2Ym2n1T2rDvNBOVVbMWxXIRo2c18JH6/c2fcKT1bRKxsG7bYq69+n9IHmKedY6rmTU1VOZZdtSTXh7exMsl67IAcnCZ0/ec+P+ZMpkK5v4X8d27rbEigGqqHayGe1/lG2afzgcHY7QxjJ/x5O4yGmVVs8wdzdupke19U+M8Z/x0FcYIfTAHuXcaydEL+h/w/ppcuNarD2ggo2BxdWeOGLx5GOin1yruVfvDAazPEuI0m3yEwXQNZ4e0ar2G0jDCZpAJPJcJRRVttBMwPoAvzTwySUx3qI1eNSJRAH+bk";
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
         parameters.cameraDirection = CAMERA_CHOICE;
+
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
 
 
-        VuforiaTrackables calculatorTarget = vuforia.loadTrackablesFromAsset("Calculator");
-        calc = calculatorTarget.get(0);
-        calc.setName("Calculator");  // Calculator
-
-        /**
-         * Load the data set containing rover ruckus targets
-         * @see VuMarkInstanceId
-         */
-        // Load the data sets that for the trackable objects. These particular data
-        // sets are stored in the 'assets' part of our application.
-        VuforiaTrackables targetsRoverRuckus = vuforia.loadTrackablesFromAsset("RoverRuckus");
-        blueRover = targetsRoverRuckus.get(0);
+        //Trackables
+        VuforiaTrackables targetsRoverRuckus = this.vuforia.loadTrackablesFromAsset("RoverRuckus");
+        VuforiaTrackable blueRover = targetsRoverRuckus.get(0);
         blueRover.setName("Blue-Rover");
-        redFootprint = targetsRoverRuckus.get(1);
+        VuforiaTrackable redFootprint = targetsRoverRuckus.get(1);
         redFootprint.setName("Red-Footprint");
-        frontCraters = targetsRoverRuckus.get(2);
+        VuforiaTrackable frontCraters = targetsRoverRuckus.get(2);
         frontCraters.setName("Front-Craters");
-        backSpace = targetsRoverRuckus.get(3);
+        VuforiaTrackable backSpace = targetsRoverRuckus.get(3);
         backSpace.setName("Back-Space");
 
 
-        List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
         allTrackables.addAll(targetsRoverRuckus);
-        allTrackables.add(calc);
 
-        /**
-         * To place the BlueRover target in the middle of the blue perimeter wall:
-         * - First we rotate it 90 around the field's X axis to flip it upright.
-         * - Then, we translate it along the Y axis to the blue perimeter wall.
-         */
-        OpenGLMatrix blueRoverLocationOnField = OpenGLMatrix
+
+        //Setting the Location of the Trackables
+        OpenGLMatrix blueRoverLocation = OpenGLMatrix
                 .translation(0, mmFTCFieldWidth, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 0));
-        blueRover.setLocation(blueRoverLocationOnField);
+        blueRover.setLocation(blueRoverLocation);
 
-        /**
-         * To place the RedFootprint target in the middle of the red perimeter wall:
-         * - First we rotate it 90 around the field's X axis to flip it upright.
-         * - Second, we rotate it 180 around the field's Z axis so the image is flat against the red perimeter wall
-         *   and facing inwards to the center of the field.
-         * - Then, we translate it along the negative Y axis to the red perimeter wall.
-         */
-        OpenGLMatrix redFootprintLocationOnField = OpenGLMatrix
+
+        OpenGLMatrix redFootprintLocation = OpenGLMatrix
                 .translation(0, -mmFTCFieldWidth, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 180));
-        redFootprint.setLocation(redFootprintLocationOnField);
+        redFootprint.setLocation(redFootprintLocation);
 
-        /**
-         * To place the FrontCraters target in the middle of the front perimeter wall:
-         * - First we rotate it 90 around the field's X axis to flip it upright.
-         * - Second, we rotate it 90 around the field's Z axis so the image is flat against the front wall
-         *   and facing inwards to the center of the field.
-         * - Then, we translate it along the negative X axis to the front perimeter wall.
-         */
-        OpenGLMatrix frontCratersLocationOnField = OpenGLMatrix
+
+        OpenGLMatrix frontCratersLocation = OpenGLMatrix
                 .translation(-mmFTCFieldWidth, 0, mmTargetHeight)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0 , 90));
-        frontCraters.setLocation(frontCratersLocationOnField);
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 90));
+        frontCraters.setLocation(frontCratersLocation);
 
-        /**
-         * To place the BackSpace target in the middle of the back perimeter wall:
-         * - First we rotate it 90 around the field's X axis to flip it upright.
-         * - Second, we rotate it -90 around the field's Z axis so the image is flat against the back wall
-         *   and facing inwards to the center of the field.
-         * - Then, we translate it along the X axis to the back perimeter wall.
-         */
-        OpenGLMatrix backSpaceLocationOnField = OpenGLMatrix
+
+        OpenGLMatrix backSpaceLocation = OpenGLMatrix
                 .translation(mmFTCFieldWidth, 0, mmTargetHeight)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, -90));
-        backSpace.setLocation(backSpaceLocationOnField);
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 270));
+        backSpace.setLocation(backSpaceLocation);
 
-    /*   float backPerimeterRotZ = 270;*/
 
-        /**
-         * Create a transformation matrix describing where the phone is on the robot.
-         *
-         * The coordinate frame for the robot looks the same as the field.
-         * The robot's "forward" direction is facing out along X axis, with the LEFT side facing out along the Y axis.
-         * Z is UP on the robot.  This equates to a bearing angle of Zero degrees.
-         *
-         * The phone starts out lying flat, with the screen facing Up and with the physical top of the phone
-         * pointing to the LEFT side of the Robot.  It's very important when you test this code that the top of the
-         * camera is pointing to the left side of the  robot.  The rotation angles don't work if you flip the phone.
-         *
-         * If using the rear (High Res) camera:
-         * We need to rotate the camera around it's long axis to bring the rear camera forward.
-         * This requires a negative 90 degree rotation on the Y axis
-         *
-         * If using the Front (Low Res) camera
-         * We need to rotate the camera around it's long axis to bring the FRONT camera forward.
-         * This requires a Positive 90 degree rotation on the Y axis
-         *
-         * Next, translate the camera lens to where it is on the robot.
-         * In this example, it is centered (left to right), but 110 mm forward of the middle of the robot, and 200 mm above ground level.
-         */
-
-        final int CAMERA_FORWARD_DISPLACEMENT  = 110;   // eg: Camera is 110 mm in front of robot center
-        final int CAMERA_VERTICAL_DISPLACEMENT = 200;   // eg: Camera is 200 mm above ground
-        final int CAMERA_LEFT_DISPLACEMENT     = 0;     // eg: Camera is ON the robot's center line
+        //Phone Location
+        final int CAMERA_FORWARD_DISPLACEMENT = 0;   //Camera is 0 mm in front of robot center
+        final int CAMERA_VERTICAL_DISPLACEMENT = 155;   //Camera is 150 mm above ground
+        final int CAMERA_HORIZONTAL_DISPLACEMENT = -200;  //Camera is on the right end of the robot (phone is on the side)
 
         OpenGLMatrix phoneLocationOnRobot = OpenGLMatrix
-                .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, YZX, DEGREES,
-                        CAMERA_CHOICE == FRONT ? 90 : -90, 0, 0));
+                .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_HORIZONTAL_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES,
+                        90, 0, -90)); //since the phone is on the side of the robot
 
-        /**  Let all the trackable listeners know where the phone is.  */
-        for (VuforiaTrackable trackable : allTrackables)
-        {
-            ((VuforiaTrackableDefaultListener)trackable.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
+        //Giving the phone location to all the trackables
+        for (VuforiaTrackable trackable : allTrackables) {
+            ((VuforiaTrackableDefaultListener) trackable.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
         }
-        calculatorTarget.activate();
+
         targetsRoverRuckus.activate();
+
     }
 
-    public OpenGLMatrix getPosition()
-    {
-        // check all the trackable target to see which one (if any) is visible.
-        targetVisible = false;
-        for (VuforiaTrackable trackable : allTrackables) {
-            if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
-                //Log.d("Visible Target", trackable.getName());
-                targetVisible = true;
-
-                // getUpdatedRobotLocation() will return null if no new information is available since
-                // the last time that call was made, or if the trackable is not currently visible.
-                OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
-                if (robotLocationTransform != null) {
-                    lastLocation = robotLocationTransform;
-                }
-                break;
-            }
-        }
-
-        // Provide feedback as to where the robot is located (if we know).
-        if (targetVisible) {
-            // express position (translation) of robot in inches.
-            VectorF translation = lastLocation.getTranslation();
-            double tX = translation.get(0)/ mmPerInch;
-            double tY = translation.get(1)/ mmPerInch;
-            double tZ = translation.get(2)/ mmPerInch;
-            //Log.d("Translation in inches: ", "(" + tX + ", " + tY + ", " + tZ + ")");
-
-            // express the rotation of the robot in degrees.
-            Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
-            double rX = rotation.firstAngle;
-            double rY = rotation.secondAngle;
-            double rZ = rotation.thirdAngle;
-            //Log.d("Rotation in deg: ", "(" + rX + ", " + rY + ", " + rZ + ")");
-         }
-        else {
-            //Log.d("Visible Target", "none");
-        }
-
+    public OpenGLMatrix getLastLocation() {
         return lastLocation;
-
     }
 
     String format(OpenGLMatrix transformationMatrix) {
@@ -253,25 +152,7 @@ public class Location {
         return false;
     }
 
-    public VuforiaTrackable getCalc() {
-        return calc;
-    }
 
-    public VuforiaTrackable getBlueRover() {
-        return blueRover;
-    }
-
-    public VuforiaTrackable getRedFootprint() {
-        return redFootprint;
-    }
-
-    public VuforiaTrackable getFrontCraters() {
-        return frontCraters;
-    }
-
-    public VuforiaTrackable getBackSpace() {
-        return backSpace;
-    }
 
     public List<VuforiaTrackable> getAllTrackables() {
         return allTrackables;
