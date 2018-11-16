f6210c7b66d63264632f894c31046f1f46e5250b
ButterflyNebula <athira.arayath@gmail.com>
2018-11-16 14:35:54 -0500

    hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -5,12 +5,19 @@ package org.firstinspires.ftc.teamcode;
  */
 
 
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+
+
+
 
 
 public class RobotHardware
@@ -31,6 +38,11 @@ public class RobotHardware
     public Servo   liftLock = null;
     public DigitalChannel bottomTouch = null;
     public DigitalChannel topTouch = null;
+    Rev2mDistanceSensor rev2mDistanceSensor;  // Hardware Device Object
+
+
+    public BNO055IMU imu = null;
+
 
     //Adding the Hardware Map
     private HardwareMap hwMap  = null;
@@ -66,10 +78,32 @@ public class RobotHardware
         leftIntake.setDirection(CRServo.Direction.FORWARD);
         rightIntake.setDirection(CRServo.Direction.REVERSE);
 
+
+        rev2mDistanceSensor = hwMap.get(Rev2mDistanceSensor.class, "distance_sensor");
+
+
         //Initializing the Arm motors
-        shoulder = hwMap.get(DcMotor.class, "shoulder");
-        elbow  = hwMap.get(Servo.class, "elbow");
-        wrist = hwMap.get(Servo.class, "wrist");
+       // shoulder = hwMap.get(DcMotor.class, "shoulder");
+       // elbow  = hwMap.get(Servo.class, "elbow");
+        //wrist = hwMap.get(Servo.class, "wrist");
+
+
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "FTC13747.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
 
     }
 
